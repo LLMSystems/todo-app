a155e19c22802a5e3463626737a1b89f53c72d58
max <milk333445@gmail.com>
2026-04-25 17:06:38 +0800

    feat: update API URL to Zeabur

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://todo-app-nu-one-94.vercel.app",  # ← 你的 Vercel 網址
+        "https://todo-frontend.zeabur.app",  # ← 你的 Vercel 網址
     ],
     allow_credentials=True,
     allow_methods=["*"],
--- frontend/src/api/todos.js
+++ frontend/src/api/todos.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://todo-app-tthp.onrender.com',
+  baseURL: 'https://todo-backend1.zeabur.app',
 })
 
 export const getTodos = () => api.get('/todos')
 export const createTodo = (title) => api.post('/todos', { title })
 export const deleteTodo = (id) => api.delete(`/todos/${id}`)
+
